072913189f2946b459f7deaa900ad2a6f3c08c75
weslhen <weslhenbakun@gmail.com>
2026-07-19 01:21:00 -0300

    Define cadastroVIEW como tela principal

--- src/cadastroVIEW.java
+++ src/cadastroVIEW.java
@@ -7,6 +7,7 @@
  *
  * @author Adm
  */
+
 public class cadastroVIEW extends javax.swing.JFrame {
 
     /**
@@ -162,8 +163,8 @@ public class cadastroVIEW extends javax.swing.JFrame {
     /**
      * @param args the command line arguments
      */
-    public static void main(String args[]) {
-        /* Set the Nimbus look and feel */
+public static void main(String args[]) {
+    // Sistema de Leilões - tela inicial cadastroVIEW
         //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
         /* If Nimbus (introduced in Java SE 6) is not available, stay with the default look and feel.
          * For details see http://download.oracle.com/javase/tutorial/uiswing/lookandfeel/plaf.html 
@@ -186,8 +187,9 @@ public class cadastroVIEW extends javax.swing.JFrame {
         }
         //</editor-fold>
 
-        /* Create and display the form */
-        java.awt.EventQueue.invokeLater(new Runnable() {
+/* Create and display the form
+   Tela principal definida como cadastroVIEW conforme requisito da atividade 2 */
+java.awt.EventQueue.invokeLater(new Runnable() {
             public void run() {
                 new cadastroVIEW().setVisible(true);
             }
