70f1baa3e519898ed3c537a0a920c28833c7cb41
weslhen <weslhenbakun@gmail.com>
2026-07-19 00:56:13 -0300

    Primeiro commit do projeto LeiloesTDSat

---
+++ src/ProdutosDAO.java
@@ -0,0 +1,59 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author Adm
+ */
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.ArrayList;
+import javax.swing.JOptionPane;
+
+public class ProdutosDAO {
+
+    Connection conn;
+    PreparedStatement prep;
+    ResultSet resultset;
+    ArrayList<ProdutosDTO> listagem = new ArrayList<>();
+
+    public void cadastrarProduto(ProdutosDTO produto) {
+
+        String sql = "INSERT INTO produtos (nome, valor, status) VALUES (?, ?, ?)";
+
+        try {
+
+            conn = new conectaDAO().connectDB();
+
+            prep = conn.prepareStatement(sql);
+
+            prep.setString(1, produto.getNome());
+            prep.setInt(2, produto.getValor());
+            prep.setString(3, produto.getStatus());
+
+            prep.executeUpdate();
+
+            JOptionPane.showMessageDialog(null, "Produto cadastrado com sucesso!");
+
+            prep.close();
+            conn.close();
+
+        } catch (Exception e) {
+
+            JOptionPane.showMessageDialog(null, "Erro ao cadastrar produto:\n" + e.getMessage());
+
+        }
+
+    }
+
+    public ArrayList<ProdutosDTO> listarProdutos() {
+
+        return listagem;
+
+    }
+
+}
---
+++ src/conectaDAO.java
@@ -0,0 +1,29 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import javax.swing.JOptionPane;
+
+public class conectaDAO {
+
+    public Connection connectDB() {
+
+        Connection conn = null;
+
+        try {
+
+            conn = DriverManager.getConnection(
+                "jdbc:mysql://localhost:3306/uc11",
+                "root",
+                "99354085"
+            );
+
+        } catch (SQLException erro) {
+
+            JOptionPane.showMessageDialog(null,
+                    "Erro ao conectar ao banco:\n" + erro.getMessage());
+
+        }
+
+        return conn;
+    }
+}
